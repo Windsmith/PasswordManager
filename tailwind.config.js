5fdb103bc1a6dcfd6bc2b395c3ff5dacc869f49a
Shriram <moonlight.shriram@gmail.com>
2021-02-14 16:39:41 +0400

    Fixed size of popup and added more values to css file for minheight

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      minHeight:{
+        '32':'8rem',
+        '36':'9rem',
+        '40':'10rem',
+        '44':'11rem',
+        '48':'12rem',
+        '52':'13rem',
+        '56':'14rem',
+        '60':'15rem'
+      }
+    },
   },
   variants: {
     extend: {},
